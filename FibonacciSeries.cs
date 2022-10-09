92ddb3bd7edd913c2bb282c17f7b98881120ffe7
himanshu01031997 <hs010397@gmail.com>
2022-10-09 11:05:21 +0530

    add:added fibonacci series

---
+++ FibonacciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public static class FibonacciSeries
+    {
+        static int n1 = 0, n2 = 1, n3,num;
+        public static void CalculateFibonacci()
+        {
+            Console.WriteLine("enter the number of element");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.Write(n1 + " " + n2 + " " );
+            for(int i = 2; i < num; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 +" ");
+                n1 = n2;
+                n2 = n3;
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace LogicalProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter your choice");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("1.fibonacci series\n");
+            int choice= Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    FibonacciSeries.CalculateFibonacci();
+                    break;
+                    default:
+                    break;
+
+            }
+            // FibonacciSeries.CalculateFibonacci();
+            //PerfectNo.CheckPerfectno();
+           // ReverseNo.findReverse();
+        }
+    }
+}
